07bd604d2a3a1ff5cbf6e3c9db2ff08100454ffd
Nour-Cheour10 <nour.cheour@insat.ucar.tn>
2024-04-18 16:49:37 +0200

    manip traitlet attribute

--- Ipyopenlayers.py
+++ Ipyopenlayers.py
@@ -15,7 +15,9 @@ except importlib.metadata.PackageNotFoundError:
 class Map (anywidget.AnyWidget):  
     _esm = pathlib.Path("src/IpyOpenLayer_repr/static/widget.js")
     _css = pathlib.Path("src/IpyOpenLayer_repr/static/widget.css")
-    DIMS = tt.Unicode("600px").tag(sync=True)
+    DIMS = tt.Unicode("700px").tag(sync=True)
+
+  
 
 '''    def __init__(self):
         super().__init__()
--- js/widget.js
+++ js/widget.js
@@ -24,18 +24,24 @@ function render({ model, el }) {
 }
 export default { model, el };
 */
+
 import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 import 'ol/ol.css';
 
+
 function render({ model, el }) {
-     let DIMS = () => model.get("DIMS")
-     console.log(`The 'my_value' changed to: ${DIMS}`);
-     el.style.height = DIMS;
-     console.log(`height: ${el.style.height}`)
-       const map = new Map({
+     function on_change() {   
+          let DIMS = () => model.get("DIMS")
+          console.log(`The 'my_value' changed to: ${DIMS}`);
+          el.style.height = DIMS;
+          console.log(`height: ${el.style.height}`)
+       }
+     model.on("change:DIMS", on_change);
+
+          const map = new Map({
         layers: [
           new TileLayer({
             source: new OSM(),
@@ -50,7 +56,7 @@ function render({ model, el }) {
         console.log(el.style.height);
         console.log("Hello");
 
-       /*console.log(map.getAllLayers());*/
+       /console.log(map.getAllLayers());/
 }
 
 export default { render };
